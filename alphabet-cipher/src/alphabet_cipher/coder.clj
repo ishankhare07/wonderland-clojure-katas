c0fa76d19d7ab1037d27efa6a657d7ec6eabeae2
Ishan Khare <ishankhare07@gmail.com>
2019-04-11 04:49:26 +0530

    reimplement and simplify encode
    Signed-off-by: Ishan Khare <ishankhare07@gmail.com>

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,20 +1,24 @@
-(ns alphabet-cipher.coder)
+(ns alphabet-cipher.coder
+  (:use [clojure.string :only [index-of]]))
 
-(defn shift
-  [k m]
-  (let [char-case 97
-        after-shift (+ (int m) (- (int k) char-case))]
-    (if (>= after-shift (+ char-case 26))
-      (char (- (int k) (- (+ char-case 26) (int m))))
-      (char after-shift))))
+(defn char-map []
+  (map char (range 97 (+ 97 26))))
+
+(defn cipher [m k]
+  (let [chars (apply str (char-map))
+        mx (index-of chars m)
+        kx (index-of chars k)
+        r (+ mx kx)]
+    (if (< r 26)
+      (nth chars r)
+      (do
+        (nth chars (rem r 26))))))
 
 (defn encode [keyword message]
-  (apply str (map shift (cycle keyword) message)))
+  (apply str (map cipher (cycle keyword) message)))
 
 (defn decode [keyword message]
   "decodeme")
 
 (defn decipher [cipher message]
   "decypherme")
-
-(apply str (map shift "apple" "mango"))
