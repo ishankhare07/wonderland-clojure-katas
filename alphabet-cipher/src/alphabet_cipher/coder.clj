3c585eeceb15b3a02d7f15c5a4756e31bd4786e8
Ishan Khare <ishankhare07@gmail.com>
2019-04-14 13:22:56 +0530

    handling repeating encryption keys
    Signed-off-by: Ishan Khare <ishankhare07@gmail.com>

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,5 +1,6 @@
 (ns alphabet-cipher.coder
-  (:use [clojure.string :only [index-of]]))
+  (:use [clojure.string :only [index-of]]
+        [clojure.math.numeric-tower :only [abs]]))
 
 (defn char-map []
   (map char (range 97 (+ 97 26))))
@@ -23,11 +24,30 @@
       (nth chars (+ 26 m))
       (nth chars m))))
 
+(defn retrieve [c m]
+  (let [chars (apply str (char-map))
+        cx (index-of chars c)
+        mx (index-of chars m)
+        k (- cx mx)]
+    (if (< k 0)
+      (nth chars (+ 26 k))
+      (nth chars k))))
+
 (defn encode [keyword message]
   (apply str (map encrypt (cycle keyword) message)))
 
 (defn decode [keyword message]
   (apply str (map decrypt (cycle keyword) message)))
 
+(defn reduce-repeat
+  [current-key reduced-key]
+  (println current-key reduced-key)
+  (let [m (first (re-seq #"(.+)\1" current-key))]
+    (if (= (last m) nil)
+      (do
+        (println m reduced-key)
+        current-key)
+      (reduce-repeat (last m) (first m)))))
+
 (defn decipher [cipher message]
-  "decypherme")
+  (reduce-repeat (apply str (map retrieve cipher message)) nil))
